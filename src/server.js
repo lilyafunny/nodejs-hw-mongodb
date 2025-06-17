23a29316662cc8207492dd4aec9b9156ab9299cf
lilyafunny <lilyafunny21@gmail.com>
2025-06-17 14:52:20 +0200

    first part

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { randomUUID } from 'node:crypto';
 import cors from 'cors';
 import pino from 'pino-http';
 import { getEnvVar } from './utils/getEnvVar.js';
 import { ENV_VARS } from './constants/envVars.js';
-import { getAllContacts, getContactById } from './services/contacts.js';
+import ContactsRouter from './routers/contacts.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
 export const setupServer = () => {
     const app = express();
@@ -21,62 +22,16 @@ export const setupServer = () => {
 
     ),);
 
-    app.use((req, res, next) => {
-        req.id = randomUUID();
-        next();
-    });
-
     app.get('/', (req, res, next) => {
-        res.status(200).json({
+        res.json({
             status: 200,
             message: 'Hello',
         });
     });
 
-    app.get('/contacts', async (req, res) => {
-        const contacts = await getAllContacts();
-
-        res.status(200).json({
-            status: 200,
-            message: 'Successfully found contacts!',
-            data: contacts,
-        });
-    });
-
-    app.get('/contacts/:contactId', async (req, res, next) => {
-        const { contactId } = req.params;
-        const contact = await getContactById(contactId);
-
-        if (!contact) {
-            res.status(404).json({
-                status: 404,
-                message: 'Contact not found'
-            });
-            return;
-        }
-
-        res.status(200).json({
-            status: 200,
-            message: `Successfully found contact with id ${contactId}!`,
-            data: contact,
-        });
-
-    });
-
-    app.use((req, res) => {
-        res.status(404).json({
-            status: 404,
-            message: 'Not Found',
-        });
-    });
-
-    app.use((err, req, res, next) => {
-        res.status(500).json({
-            status: 500,
-            message: 'Something went wrong',
-            error: err.message,
-        });
-    });
+    app.use(ContactsRouter);
+    app.use(notFoundHandler);
+    app.use(errorHandler);
 
     const PORT = getEnvVar(ENV_VARS.PORT, 3000);
 
